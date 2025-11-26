33a9cb13487dc16f59308a9b949d64a7573f22e4
Govardhan Raju <gsurapur@gmail.com>
2025-11-26 09:30:07 +0530

    session-30

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -2,29 +2,24 @@ resource "aws_instance" "terraform" {
     ami = var.ami
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.allow_all.id]
-
-    tags = {
-      Name = "terraform"
-      Terraform = true
-      Description = "practice"
+    tags = var.ec2_tags
     }
-}
 
 resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
+    name = var.security_group_name
 
 egress {
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  
+  from_port      = var.egress_from_port
+  to_port        = var.egress_to_port
+  protocol       = var.protocol
+  cidr_blocks    = var.cidr_blocks
 }
+
  ingress {
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  from_port       = var.ingress_from_port
+  to_port         = var.ingress_to_port
+  protocol        = var.protocol
+  cidr_blocks     = var.cidr_blocks
 }
 
     tags = {
