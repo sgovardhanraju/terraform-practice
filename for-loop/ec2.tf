99b1412f2712148332886bfac1fe9925dea986e0
Govardhan Raju <gsurapur@gmail.com>
2025-11-26 21:43:17 +0530

    session-31

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -1,11 +1,13 @@
 resource "aws_instance" "terraform" {
-    for_each = var.instances
+  for_each = var.instances
+  # for_each = toset(var.instances)
     ami = "ami-09c813fb71547fc4f"
-    instance_type = each.value
+  instance_type = each.key
+  #instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
 
     tags = {
-      Name = each.key
+      Name = each.value
       Terraform = true
       Description = "practice"
     }
