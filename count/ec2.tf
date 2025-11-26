8163bf424d14240e44486c421a4a2563e1171d89
Govardhan Raju <gsurapur@gmail.com>
2025-11-26 13:49:37 +0530

    session-30

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "terraform" {
-    count = 10
+    count = 4
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
 
     tags = {
-      Name = "terraform"
+      Name = var.instances[count.index]
       Terraform = true
       Description = "practice"
     }
