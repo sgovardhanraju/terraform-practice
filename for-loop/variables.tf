5bc11e0021239bc1b64cb4c7a292b434de9763ff
Govardhan Raju <gsurapur@gmail.com>
2025-11-26 19:09:14 +0530

    session-31

---
+++ for-loop/variables.tf
@@ -0,0 +1,17 @@
+variable "instances" {
+   # default = ["mongodb","mysql","rabbitmq","redis","catalogue","cart","user","shipping","payment","frontend"]
+    default = {
+        mongodb = "t3.micro"
+        mysql = "t3.small"
+        redis = "t3.micro"
+        rabbitmq = "t3.micro"
+    }
+}
+
+variable "zone_id" {
+    default = "Z0732355102QE6GB8XDYY"
+}
+
+variable "domain_name" {
+    default ="sgrdevsecops.fun"
+}
