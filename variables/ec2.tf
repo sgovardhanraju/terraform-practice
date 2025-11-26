7187fea270aa7c8f9ee9a3202f254543c0a04042
Govardhan Raju <gsurapur@gmail.com>
2025-11-26 08:41:31 +0530

    session-30

---
+++ variables/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "terraform" {
+    ami = var.ami
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+    tags = {
+      Name = "terraform"
+      Terraform = true
+      Description = "practice"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+
+egress {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  
+}
+ ingress {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+    tags = {
+      Name = "allow-all"
+    }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
