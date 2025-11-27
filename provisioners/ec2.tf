9706f18dec509931a24747417d0617b79e73f8d6
Govardhan Raju <gsurapur@gmail.com>
2025-11-27 16:37:51 +0530

    session-32

---
+++ provisioners/ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+    tags = {
+      Name = "terraform-1"
+      Terraform = true
+    }
+
+    provisioner "local-exec" {
+      command = "echo ${self.private_ip} >> inventory"
+      on_failure = continue
+    }
+
+    provisioner "local-exec" {
+      command = "echo ${self.public_ip} >> inventory"
+      on_failure = continue
+    }
+
+    provisioner "local-exec" {
+      command = "echo Instacne is destroyed"
+       when = destroy
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+
+egress {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  
+}
+ ingress {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+    tags = {
+      Name = "allow-all"
+    }
+}
